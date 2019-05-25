33f10b4ec82ebaddeccbe15c868bf870c0d19952
CJcopeland20 <copelchr@kean.edu>
2019-05-24 21:06:49 -0400

    fix post changes

--- Project2Server/src/main/java/com/revature/controllers/RecipeController.java
+++ Project2Server/src/main/java/com/revature/controllers/RecipeController.java
@@ -6,12 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,13 +32,13 @@ private RecipeServices recipeService;
 		this.recipeService = recipeService;
 	}*/
 
-	@GetMapping("/{id}")
+	@PostMapping("/{id}")
 	public Recipe getById(@PathVariable int id) {
 		return Optional.ofNullable(this.recipeService.getById(id))
 			.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
 	}
 	
-	@GetMapping("/search/")
+	@PostMapping("/search/")
 	public List<Recipe> searchByName(@RequestBody SearchDTO search) {
 		List<Recipe> toRet = this.recipeService.getByName(search.getRecipe());
 		if(toRet == null)
